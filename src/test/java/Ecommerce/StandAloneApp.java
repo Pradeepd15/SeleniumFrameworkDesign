2a8e9e410a3bd367be2c63c0ff3da1e4eb139f64
pradeep <prajapatipradeep083@gmail.com>
2025-11-11 15:42:47 +0530

    adding code in standaloneapp

--- src/test/java/Ecommerce/StandAloneApp.java
+++ src/test/java/Ecommerce/StandAloneApp.java
@@ -1,9 +1,18 @@
 package Ecommerce;
-
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 public class StandAloneApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/client/#/auth/login");
+		driver.findElement(By.id("userEmail")).sendKeys("pintuprajapati162@gmail.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Pradeep@123");
+		driver.findElement(By.id("login")).click();
+		
 
 	}
 
